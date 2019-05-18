7580d3fc54e3202e37e129b8bf4288b70fa3dc89
yonasGeb <37456780+yonasGeb@users.noreply.github.com>
2019-05-18 10:42:53 +0100

    added variables

---
+++ main.js
@@ -0,0 +1,6 @@
+//ASSIGN ALL ELEMENTS
+const demold = document.getElementById('demo');
+const demoClass = document.getElementsByClassName('demo');
+const demoTag = document.getElementsByTagName('article');
+const demoQuery = document.querySelector('#demo-query');
+const demoQueryAll = document.querySelectorAll('.demo-quert-all');
