ffac0ea9422c7476e41ff8d166e24be141a06e06
yonasGeb <37456780+yonasGeb@users.noreply.github.com>
2019-05-18 12:27:22 +0100

    All the code for this part of the tutorial added

--- main.js
+++ main.js
@@ -3,4 +3,20 @@ const demold = document.getElementById('demo');
 const demoClass = document.getElementsByClassName('demo');
 const demoTag = document.getElementsByTagName('article');
 const demoQuery = document.querySelector('#demo-query');
-const demoQueryAll = document.querySelectorAll('.demo-quert-all');
+const demoQueryAll = document.querySelectorAll('.demo-query-all');
+//CHANGE BORDER OF ID demo TO PURPLE
+demold.style.border = '1px solid purple';
+//CHANGE BORDER OF CLASS demo TO ORANGE
+for(let i=0 ;i<demoClass.length;i++){
+	demoClass[i].style.border = '1px solid orange';
+}
+//CHANGE BORDER OF TAG demo TO BLUE
+for (let i =0;i < demoTag.length; i++){
+	demoTag[i].style.border = '1px solid blue';
+	}
+//CHANGE BORDER OF ID demo-query TO RED
+demoQuery.style.border = '1px solid red';
+//CHANGE BORDER OF demoQueryAll TO GREEN
+for (let index = 0; index < demoQueryAll.length; index++) {
+	demoQueryAll[index].style.border = '1px solid green';
+}
